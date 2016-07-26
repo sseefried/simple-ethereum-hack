7f844e1090024711eef219f21a611ad766eb2061
Sean Seefried <sean.seefried@gmail.com>
2016-07-26 16:17:15 +1000

    Add THIS_DIR and use it to attach.sh

--- attach.sh
+++ attach.sh
@@ -1,3 +1,7 @@
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATADIR=$THIS_DIR/data
+########
+
 GETH="$HOME/code/eth/go-ethereum/build/bin/geth"
 $GETH attach http://localhost:8545
 
